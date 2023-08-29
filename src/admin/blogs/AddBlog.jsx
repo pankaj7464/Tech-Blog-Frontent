aac17279c48f8dd9faa5d4e0ffc5b0a752b0ca23
pankaj7464 <pkumar44676@gmail.com>
2023-08-29 22:41:10 +0530

    delete junk componants

--- src/admin/blogs/AddBlog.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const AddBlog = () => {
-  return (
-    <div>AddBlog</div>
-  )
-}
--- src/admin/categories/AllCategories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AllCategories = () => {
-  return (
-    <div>AllCategories</div>
-  )
-}
-
-export default AllCategories
